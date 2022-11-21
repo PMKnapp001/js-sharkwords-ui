46309e8cece008d05d65f7a4a1996f03906a8bc7
PMKnapp001 <pmknapp001@outlook.com>
2022-11-21 12:51:34 -0500

    completed ui

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -20,13 +20,18 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const letter of word) {
+    //console.log(letter);
+    document.querySelector('#word-container').insertAdjacentHTML('beforeend', `<div class="letter-box ${letter}"></div>`);
+  }
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+  for(const letter of ALPHABET) {
+    document.querySelector('#letter-buttons').insertAdjacentHTML('beforeend', `<button>${letter}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,7 +39,7 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  buttonEl.setAttribute('disabled', 'true');
 };
 
 // This is a helper function we will use in the future
@@ -42,7 +47,10 @@ const disableLetterButton = (buttonEl) => {
 // For now, you should test it out to make sure it works
 
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+  if (document.querySelector(`.${letter}`) === null) {
+    return false;
+  }
+  return true;
 };
 
 // This is like if __name__ == '__main__' in Python
@@ -53,13 +61,13 @@ const isLetterInWord = (letter) => {
   // You can change this to choose a random word from WORDS once you
   // finish this lab but we hard code it so we know what the word is
   // and can tell if things look correct for this word
-  const word = 'hello';
+  const word = WORDS[Math.floor(Math.random()*WORDS.length)]; //Referred to https://www.w3schools.com/js/js_random.asp for random int help
 
   // call the function that makes an empty line for each letter in the word
-  // Replace this line with the function call
+  createDivsForChars(word);
 
   // call the function that makes a button for each letter in the alphabet
-  // Replace this line with the function call
+  generateLetterButtons();
 
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
